042ab9e8bedb36df35d7a8fc39e4e30863e52f98
anar <anarsa-bp216@code.edu.az>
2024-11-01 18:28:30 +0400

    completed task

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const holdBtn = document.getElementById("hold");
 const newGameBtn = document.getElementById("newgame");
 const diceImage = document.getElementById("diceImage");
 
+const player1 = document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
+
 let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
@@ -16,17 +19,28 @@ let gamePlaying = true;
 function switchPlayer() {
   currentScore = 0;
   document.querySelector(`.currentscore${activePlayer + 1}`).textContent = 0;
+
+  document
+    .querySelector(`.player${activePlayer + 1}`)
+    .classList.remove("active-player");
+
   activePlayer = activePlayer === 0 ? 1 : 0;
+  document
+    .querySelector(`.player${activePlayer + 1}`)
+    .classList.add("active-player");
 }
 
 function rollDice() {
   if (gamePlaying) {
     const dice = Math.floor(Math.random() * 6) + 1;
-    diceImage.src = `./img/dice${dice}.jpeg`; // Zar görselini günceller
-
+    diceImage.src = `./img/dice${dice}.jpeg`;
+    document
+      .querySelector(`.player${activePlayer + 1}`)
+      .classList.add("active-player");
     if (dice !== 1) {
       currentScore += dice;
-      document.querySelector(`.currentscore${activePlayer + 1}`).textContent = currentScore;
+      document.querySelector(`.currentscore${activePlayer + 1}`).textContent =
+        currentScore;
     } else {
       switchPlayer();
     }
@@ -36,11 +50,14 @@ function rollDice() {
 function hold() {
   if (gamePlaying) {
     scores[activePlayer] += currentScore;
-    document.querySelector(`.score${activePlayer + 1}`).textContent = scores[activePlayer];
+    document.querySelector(`.score${activePlayer + 1}`).textContent =
+      scores[activePlayer];
 
     if (scores[activePlayer] >= 100) {
       gamePlaying = false;
-      document.querySelector(`.name${activePlayer + 1}`).textContent = `PLAYER ${activePlayer + 1} WINS!`;
+      document.querySelector(
+        `.name${activePlayer + 1}`
+      ).textContent = `PLAYER ${activePlayer + 1} WINS!`;
     } else {
       switchPlayer();
     }
@@ -57,9 +74,11 @@ function newGame() {
   player2ScoreElem.textContent = 0;
   current1Elem.textContent = 0;
   current2Elem.textContent = 0;
-  diceImage.src = 'dice-1.png';
+  diceImage.src = "./img/dice1.jpeg";
   document.querySelector(`.name1`).textContent = "PLAYER 1";
   document.querySelector(`.name2`).textContent = "PLAYER 2";
+  document.querySelector(`.player1`).classList.remove("active-player");
+  document.querySelector(`.player2`).classList.remove("active-player");
 }
 
 rollDiceBtn.addEventListener("click", rollDice);
